91c81a1930ebcdfc28c432750238fec57828bbc3
Nagaku <mimelnagaku1@gmail.com>
2021-05-29 13:28:45 +0700

    ok

--- app/src/main/java/com/example/quiz10118008/MainActivity.java
+++ app/src/main/java/com/example/quiz10118008/MainActivity.java
@@ -8,14 +8,26 @@ package com.example.quiz10118008;
  */
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = findViewById(R.id.kotak1);
+        Adapter adapter = new Adapter();
+
+        GridLayoutManager layoutManager=new GridLayoutManager(this,5);
+
+        rv.setLayoutManager(layoutManager);
+        rv.setAdapter(adapter);
     }
 }
